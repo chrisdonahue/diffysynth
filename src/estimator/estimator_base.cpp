fed16d92d9e6ef337d536c8dc3425485ccf2ed76
Chris Donahue <christopherdonahue@gmail.com>
2015-04-29 16:29:10 -0700

    restored estimators

---
+++ src/estimator/estimator_base.cpp
@@ -0,0 +1,1 @@
+#include "estimator_base.hpp"
---
+++ src/estimator/estimator_base.hpp
@@ -0,0 +1,26 @@
+#ifndef DIFFYSYNTH_ESTIMATOR_BASE
+#define DIFFYSYNTH_ESTIMATOR_BASE
+
+#include <array>
+
+#include "../type.hpp"
+
+#include "../primitive/primitive_base.hpp"
+
+namespace diffysynth { namespace estimator {
+	class base {
+	public:
+		class state {
+		public:
+		protected:
+		private:
+			std::array<type::diff, 
+		};
+
+		base();
+	protected:
+	private:
+	};
+}}
+
+#endif
