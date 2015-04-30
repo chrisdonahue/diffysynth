f012be2eddff5e12395e42b2a4b9e081cfbc357d
Chris Donahue <christopherdonahue@gmail.com>
2015-04-30 16:20:40 -0700

    gp stuff

--- src/gp/evaluator.cpp
+++ src/gp/evaluator.cpp
@@ -1,65 +1,6 @@
-#include "diff_eq_system.hpp"
+#include "evaluator.hpp"
 
 using namespace diffysynth;
 
-diff_eq_system::diff_eq_system(type::id _diff_eqs_num, type::id _parameters_num) :
-	diff_eqs_num(_diff_eqs_num),
-	diff_eqs(diff_eqs_num),
-	parameters_num(_parameters_num)
-{
-	for (type::id identifier = 0; identifier < diff_eqs_num; identifier++) {
-		diff_eqs[identifier] = nullptr;
-	}
-};
-
-diff_eq_system::diff_eq_system(const diff_eq_system& other) :
-	diff_eqs_num(other.diff_eqs_num),
-	diff_eqs(diff_eqs_num),
-	parameters_num(other.parameters_num)
-{
-	for (type::id identifier = 0; identifier < diff_eqs_num; identifier++) {
-		diff_eq* other_diff_eq = other.diff_eqs[identifier];
-		if (other_diff_eq == nullptr) {
-			diff_eqs[identifier] = nullptr;
-		}
-		else {
-			diff_eqs[identifier] = new diff_eq(const_cast<diff_eq&>(*other_diff_eq));
-		}
-	}
-};
-
-type::id diff_eq_system::diff_eqs_num_get() const {
-	return diff_eqs_num;
-};
-
-type::id diff_eq_system::parameters_num_get() const {
-	return parameters_num;
-};
-
-void diff_eq_system::diff_eq_set(type::id identifier, diff_eq* instance) {
-#ifdef DIFFYSYNTH_DEBUG_API
-	ensure(identifier < diff_eqs_num);
-#endif
-
-	diff_eqs[identifier] = instance;
-};
-
-diff_eq* diff_eq_system::diff_eq_get(type::id identifier) {
-#ifdef DIFFYSYNTH_DEBUG_API
-	ensure(identifier < diff_eqs_num);
-#endif
-
-	return diff_eqs[identifier];
-};
-
-void diff_eq_system::evaluate(evaluate_signature) const {
-#ifdef DIFFYSYNTH_DEBUG_API
-	for (type::id identifier = 0; identifier < diff_eqs_num; identifier++) {
-		ensure(diff_eqs[identifier] != nullptr);
-	}
-#endif
-
-	for (type::id identifier = 0; identifier < diff_eqs_num; identifier++) {
-		derivatives[identifier] = diff_eqs[identifier]->evaluate(evaluate_arguments);
-	}
-};
+gp::evaluator::evaluator()
+{};
--- src/gp/population.hpp
+++ src/gp/population.hpp
@@ -1,35 +1,20 @@
-#ifndef DIFFYSYNTH_SYSTEM
-#define DIFFYSYNTH_SYSTEM
+#ifndef DIFFYSYNTH_GP_POPULATION
+#define DIFFYSYNTH_GP_POPULATION
 
-#include <array>
-
+#include <vector>
 #include <map>
 
-#include "define.hpp"
-#include "type.hpp"
-#include "value.hpp"
+#include "../define.hpp"
+#include "../type.hpp"
+#include "../value.hpp"
 
-#include "diff_eq.hpp"
-
-namespace diffysynth {
-	class diff_eq_system {
+namespace diffysynth { namespace gp {
+	class population {
 	public:
-		diff_eq_system(type::id _diff_eqs_num, type::id _parameters_num);
-		diff_eq_system(const diff_eq_system& other);
-
-		type::id diff_eqs_num_get() const;
-		type::id parameters_num_get() const;
-
-		void diff_eq_system::diff_eq_set(type::id identifier, diff_eq* instance);
-		diff_eq* diff_eq_get(type::id identifier);
-
-		void evaluate(evaluate_signature) const;
+		population();
 
 	private:
-		type::id diff_eqs_num;
-		std::vector<diff_eq*> diff_eqs;
-		type::id parameters_num;
 	};
-}
+}}
 
 #endif
